dc5468bd35745686fac009af14de2162bec386fa
Hung Do <doyouknowed@gmail.com>
2019-04-11 19:04:11 +0700

    2nd

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!")
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if (!req.url == '/hello') return
+    const method = req.method
+    const text = {
+        GET: '',
+        POST: ' created',
+        PUT: ' updated',
+        DELETE: ' deleted'
+    }
+    const resText = text[method] || ''
+    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.write(`world${resText}`)
+    res.end()
+})
+
+const port = 5000
+server.listen(port)
+console.log(`Listening at port ${port}...`)
